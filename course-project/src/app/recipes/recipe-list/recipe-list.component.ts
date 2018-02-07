dd21ec2b3f44a14101ba10d09ff8fea293c443a3
Ujjwal Jindal <ujjwaljindal12@gmail.com>
2018-02-07 23:52:38 +0530

    components details added

--- course-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ course-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe','This is simply a test','http://media.immediate.co.uk/volatile/sites/2/2018/01/OLI0218-Healthy-MonsterSalad_020309-89240d9.jpg?quality=45&resize=556,723'),
+    new Recipe('A Test Recipe','This is simply a test','http://media.immediate.co.uk/volatile/sites/2/2018/01/OLI0218-Healthy-MonsterSalad_020309-89240d9.jpg?quality=45&resize=556,723')
+  ];
 
   constructor() { }
 
