c9407f894cf136d449f394339079511c3dd91fda
Ujjwal Jindal <ujjwaljindal12@gmail.com>
2018-02-12 22:34:57 +0530

    local variable reference in templates updated

--- cmp-databinding-start/src/app/cockpit/cockpit.component.ts
+++ cmp-databinding-start/src/app/cockpit/cockpit.component.ts
@@ -8,24 +8,24 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 export class CockpitComponent implements OnInit {
   @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
-  newServerName = '';
+  //newServerName = '';
   newServerContent = '';
   constructor() { }
 
   ngOnInit() {
   }
 
-  onAddServer(nameInput) {
-    console.log(nameInput);
+  onAddServer(nameInput: HTMLInputElement) {
+
   this.serverCreated.emit({
-    serverName: this.newServerName,
+    serverName: nameInput.value,
     serverContent: this.newServerContent
   });
 }
 
-  onAddBlueprint() {
+  onAddBlueprint(nameInput: HTMLInputElement) {
     this.blueprintCreated.emit({
-      serverName: this.newServerName,
+      serverName: nameInput.value,
       serverContent: this.newServerContent
     })
   }
